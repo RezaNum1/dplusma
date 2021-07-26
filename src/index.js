bf270805d56e0c3ca64f37a7d0cbe02f07d7d7f5
Reza <rezafh19@gmail.com>
2021-07-26 11:25:44 +0700

    Cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { ApolloServer } from "apollo-server-express";
 import express from 'express';
+import cors from 'cors'
 import { resolvers } from "../schema/resolvers";
 import { typeDefs } from "../schema/typeDefs";
 
 async function startServer() {
     const app = express()
+    app.use(cors())
     const server = new ApolloServer({
         typeDefs,
         resolvers
@@ -12,7 +14,7 @@ async function startServer() {
 
     await server.start();
 
-    server.applyMiddleware({ app });
+    server.applyMiddleware({ app, path: "/graphql", cors: false });
 
     app.listen({ port: 4000 }, () => {
         console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
