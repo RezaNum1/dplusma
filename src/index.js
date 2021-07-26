6ccb1cf28c000bf6ff4d1b6c3f1c456aa10c594d
Reza <rezafh19@gmail.com>
2021-07-26 12:21:14 +0700

    cors

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ async function startServer() {
     const app = express()
     app.use(cors())
     app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "http://localhost:4000"); // update to match the domain you will make the request from
+        res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-        res.header("Access-Control-Allow-Credentials", true);
+        res.header("Access-Control-Allow-Credentials", false);
         next();
       });
     const server = new ApolloServer({
