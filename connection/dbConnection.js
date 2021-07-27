4c1a7a89e12c30d504a1c0085c5bc21050bbf7cd
Ubuntu <ubuntu@ip-172-31-21-94.ap-southeast-1.compute.internal>
2021-07-27 13:53:21 +0000

    fix the apollo to playground

--- connection/dbConnection.js
+++ connection/dbConnection.js
@@ -1,9 +1,9 @@
 export const knex = require("knex")({
     client: "pg",
     connection: {
-        host: "dplusma.cyt2nxuztyho.ap-southeast-1.rds.amazonaws.com",
+        host: "database-1.c6fxehm23nsu.ap-southeast-1.rds.amazonaws.com",
         user: "postgres",
-        password: "plasmaDonor",
-        database: "dplusma"
+        password: "plasma2021",
+        database: "pmiDB"
     }
 });
--- src/index.js
+++ src/index.js
@@ -3,20 +3,26 @@ import express from 'express';
 import cors from 'cors'
 import { resolvers } from "../schema/resolvers";
 import { typeDefs } from "../schema/typeDefs";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core"
 
 async function startServer() {
     const app = express()
     app.use(cors())
     app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+        res.header("Access-Control-Allow-Origin", ""); // update to match the domain you will make the request from
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
         res.header("Access-Control-Allow-Credentials", false);
         next();
-      });
+        });
     const server = new ApolloServer({
         typeDefs,
-        resolvers
+        resolvers,
+        plugins: [
+            ApolloServerPluginLandingPageGraphQLPlayground({
+      // options
+    })
+  ]
     })
 
     await server.start();
