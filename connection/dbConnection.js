1174ffd49dd07634440a67380722cbdf05213be6
Reza <rezafh19@gmail.com>
2021-07-27 22:37:27 +0700

    fix

--- connection/dbConnection.js
+++ connection/dbConnection.js
@@ -1,9 +1,27 @@
 export const knex = require("knex")({
     client: "pg",
     connection: {
-        host: "database-1.c6fxehm23nsu.ap-southeast-1.rds.amazonaws.com",
+        host: "dplusma.cyt2nxuztyho.ap-southeast-1.rds.amazonaws.com",
         user: "postgres",
-        password: "plasma2021",
-        database: "pmiDB"
-    }
+        password: "plasmaDonor",
+        database: "dplusma"
+    },
+    pool: {
+        min: 2,
+        max: 6,
+        createTimeoutMillis: 3000,
+        acquireTimeoutMillis: 30000,
+        idleTimeoutMillis: 30000,
+        reapIntervalMillis: 1000,
+        createRetryIntervalMillis: 100,
+        propagateCreateError: false // <- default is true, set to false
+      }
 });
+
+
+// connection: {
+//     host: "database-1.c6fxehm23nsu.ap-southeast-1.rds.amazonaws.com",
+//     user: "postgres",
+//     password: "plasma2021",
+//     database: "pmiDB"
+// }
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ async function startServer() {
     const app = express()
     app.use(cors())
     app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", ""); // update to match the domain you will make the request from
+        res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
         res.header("Access-Control-Allow-Credentials", false);
