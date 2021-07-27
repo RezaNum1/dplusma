104392aff01152051105bfada1c12c500ffda8a3
Reza <rezafh19@gmail.com>
2021-07-27 22:52:28 +0700

    cors

--- connection/dbConnection.js
+++ connection/dbConnection.js
@@ -4,18 +4,9 @@ export const knex = require("knex")({
         host: "dplusma.cyt2nxuztyho.ap-southeast-1.rds.amazonaws.com",
         user: "postgres",
         password: "plasmaDonor",
-        database: "dplusma"
-    },
-    pool: {
-        min: 2,
-        max: 6,
-        createTimeoutMillis: 3000,
-        acquireTimeoutMillis: 30000,
-        idleTimeoutMillis: 30000,
-        reapIntervalMillis: 1000,
-        createRetryIntervalMillis: 100,
-        propagateCreateError: false // <- default is true, set to false
-      }
+        database: "dplusma",
+        ssl: { rejectUnauthorized: false }
+    }
 });
 
 
