40b2788028f2f81d9e18cf4932a22db26963ea17
Reza <rezafh19@gmail.com>
2021-08-24 11:52:01 +0700

    Get All Data

--- queries/pmiQueries.js
+++ queries/pmiQueries.js
@@ -4,25 +4,23 @@ const prisma = new PrismaClient();
 
 module.exports = {
     getAllPmi: async function getAllPmi() {
+        return prisma.pmi.findMany()
+        // var arrayData = []
         // const result = await prisma.$queryRaw`
-        //     SELECT * FROM "public"."Pmi"
-        //     `;
-        var arrayData = []
-        const result = await prisma.$queryRaw`
-                SELECT * FROM "public"."Pmi"
-                `;
-        arrayData.push( async () => {
+        //         SELECT * FROM "public"."Pmi"
+        //         `;
+        // arrayData.push( async () => {
             
-                result.forEach(async (x)  => {
-                    const jadwals = await prisma.$queryRaw`
-                        SELECT * FROM "public"."Jadwal" WHERE "branchId" = ${x.id} AND "open" = true
-                    `;
-                    x.jadwals = jadwals
-                    return x
-                })
-        })
-        console.log(arrayData)
-        return arrayData
+        //         result.forEach(async (x)  => {
+        //             const jadwals = await prisma.$queryRaw`
+        //                 SELECT * FROM "public"."Jadwal" WHERE "branchId" = ${x.id} AND "open" = true
+        //             `;
+        //             x.jadwals = jadwals
+        //             return x
+        //         })
+        // })
+        // console.log(arrayData)
+        // return arrayData
     },
     getPmi: function getPmi(id) {
         return prisma.pmi.findUnique({
